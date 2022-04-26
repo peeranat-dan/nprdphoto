425b5549cf64ffe0bfbcd622b57203ed3be7f75d
Nin <ppeeranat.d@gmail.com>
2022-04-26 20:53:15 +0700

    - Change app title - Finally remove flowbite - remove set title at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import GallerySection from "./components/Gallery/GallerySection";
 
 
 const App = () => {
-    useEffect(() => {
-        document.title = "@nprdphoto"
-    }, []);
   return (
     <div className="bg-slate-50 w-full min-h-screen max-h-fit pt-16 pb-4 px-4 lg:pt-28 lg:px-8 flex flex-col">
         <div className="text-2xl text-slate-500 hover:text-slate-600 transition-colors duration-200">
--- src/components/Calendar/EventCard.jsx
+++ src/components/Calendar/EventCard.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
 const EventCard = props => {
-    const config = props.config
-    ;
-    // const colorArray = ['indigo', 'green', 'pink']
-    // const random = Math.floor(Math.random() * (colorArray.length));
+    const config = props.config;
     const colors = {
         'indigo': 'bg-indigo-500 text-indigo-50',
         'pink': 'bg-rose-500 text-rose-50',
         'green': 'bg-teal-500 text-teal-50',
-
     };
     return (
         <div className="w-1/5 md:w-1/2 lg:w-2/5 m-2 bg-white rounded-lg shadow-xl flex items-center overflow-hidden select-none">
